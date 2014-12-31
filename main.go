6439e64a84287d30e66f023d796d5dfe35575104
9seconds <nineseconds@yandex.ru>
2014-12-31 11:57:14 +0300

    Bump version

--- main.go
+++ main.go
@@ -79,7 +79,7 @@ Options:
     -v, --debug
        Shows a debug log of command execution.`
 
-const version = "ah 0.14.1"
+const version = "ah 0.14.2"
 
 var validateBookmarkName = utils.CreateRegexp(`^[A-Za-z_]\w*$`)
 
